e28121d3904a27aa99d0808606fb65ca7c6e893c
Jesus Pedro Bapolo <jesusestudante@gmail.com>
2025-10-12 17:33:08 +0100

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,13 +17,13 @@ export default function Navbar() {
   },[])
 
   return (
-    <nav className="bg-[#F1F5F9] relative flex justify-between px-4 items-center p-4 transition-all mb-1 h-25">
+    <nav className="bg-[#F1F5F9] relative flex justify-between px-4 items-center p-4 transition-all mb-1 h-20 static">
       <div className="text-4xl">Logo</div>
       <div>
         <p onClick={handleMenu} className="cursor-pointer  font-thin">
           {menuActivo ?  <FaTimes className="text-3xl" /> : <FaBars className="text-3xl"/>}
         </p>
-        <ul className="bg-[#F1F5F9] flex flex-col gap-3.5 left-0 top-25 absolute w-full justify-center items-center border-t border-zinc-500">
+        <ul className="bg-[#F1F5F9] flex flex-col gap-3.5 left-0 top-20 absolute w-full justify-center items-center border-t border-zinc-500">
           {menuActivo ? (
             <>
               <li className="p-2">
