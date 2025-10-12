07c06fa735af1a667cdf84e2a09c7512c1fa41a8
Jesus Pedro Bapolo <jesusestudante@gmail.com>
2025-10-12 17:29:52 +0100

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FaBars } from "react-icons/fa";
 import { FaTimes } from "react-icons/fa";
 
@@ -12,15 +12,19 @@ export default function Navbar() {
     setMenuActivo(!menuActivo)
   }
 
+  useEffect(() => {
+    setMenuActivo(false)
+  },[])
+
   return (
-    <nav className="bg-[#F1F5F9] relative flex justify-between px-4 items-center p-4 transition-all mb-1">
-      <div>Logo</div>
+    <nav className="bg-[#F1F5F9] relative flex justify-between px-4 items-center p-4 transition-all mb-1 h-25">
+      <div className="text-4xl">Logo</div>
       <div>
-        <p onClick={handleMenu} className="cursor-pointer text-3xl font-thin">
-          {menuActivo ?  <FaTimes className="" /> : <FaBars />}
+        <p onClick={handleMenu} className="cursor-pointer  font-thin">
+          {menuActivo ?  <FaTimes className="text-3xl" /> : <FaBars className="text-3xl"/>}
         </p>
-        <ul className="bg-[#F1F5F9] flex flex-col gap-3.5 left-0 top-13 absolute w-full justify-center items-center border-t border-t-zinc-400">
-          {menuActivo && (
+        <ul className="bg-[#F1F5F9] flex flex-col gap-3.5 left-0 top-25 absolute w-full justify-center items-center border-t border-zinc-500">
+          {menuActivo ? (
             <>
               <li className="p-2">
                 <Link href="/">Home</Link>
@@ -29,7 +33,7 @@ export default function Navbar() {
                 <Link href="/sobre">Sobre</Link>
               </li>
             </>
-          )}
+          ): ""}
         </ul>
       </div>
     </nav>
