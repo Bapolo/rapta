0c19b58b86d8fd43d510836d73d859856a4cb937
Jesus Pedro Bapolo <jesusestudante@gmail.com>
2025-10-18 01:14:15 +0100

    update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,42 +16,61 @@ export default function Navbar({
   handleMenu,
   setMenuActivo,
 }: MenuProps) {
-
   return (
     <nav className="bg-[#F1F5F9]  flex justify-between px-4 items-center p-4 transition-all mb-1 h-20 relative">
       <div className="text-4xl">Logo</div>
-      <div>
-        <p onClick={handleMenu} className="cursor-pointer  font-thin">
+
+      <div className="md:hidden">
+        <div
+          onClick={handleMenu}
+          className="cursor-pointer  font-thin md:hidden"
+        >
           {menuActivo ? (
             <FaTimes className="text-3xl" />
           ) : (
             <FaBars className="text-3xl" />
           )}
-        </p>
+        </div>
 
-        <ul
-          className={clsx(
-            "absolute left-0 top-20 w-full flex-col justify-center items-center border-t border-b border-zinc-300 bg-[#F1F5F9] transition-all duration-500 ease-in-out md:flex",
-            `ms:${menuActivo}`
-              ? "opacity-100 translate-y-0 pointer-events-auto flex"
-              : "opacity-0 pointer-events-none",
-              
-          )}
-        >
-          {menuActivo ? (
-            <>
-              <li className="p-2 border-b border-zinc-300 w-full text-center">
-                <Link href="/" onClick={() => setMenuActivo(false)}>Home</Link>
-              </li>
-              <li className="p-2 w-full text-center">
-                <Link href="/sobre" onClick={() => setMenuActivo(false)}>Sobre</Link>
-              </li>
-            </>
-          ) : (
-            ""
-          )}
-        </ul>
+        {menuActivo ? (
+          <ul
+            className={clsx(
+              "flex-col justify-center absolute left-0 top-6/6 w-full items-center border-t border-b border-zinc-300 bg-[#F1F5F9] transition-all duration-500 ease-in-out",
+              menuActivo ? "flex" : "hidden"
+            )}
+          >
+            <li className="p-2 border-b border-zinc-300 w-full text-center">
+              <Link href="/" onClick={() => setMenuActivo(false)}>
+                Home
+              </Link>
+            </li>
+            <li className="p-2 w-full text-center">
+              <Link href="/sobre" onClick={() => setMenuActivo(false)}>
+                Sobre
+              </Link>
+            </li>
+          </ul>
+        ) : (
+          ""
+        )}
       </div>
+
+      <ul
+        className={
+          "hidden md:flex gap-4 bg-[#F1F5F9] font-medium text-xl"
+        }
+      >
+        <li className="text-center border border-zinc-300 rounded-md hover:bg-[#eceef0] transition duration-250 ease-in-out">
+          <Link href="/" className="flex justify-center items-center py-2 px-4 curosr-pointer" onClick={() => setMenuActivo(false)}>
+            Home
+          </Link>
+        </li>
+        <li className="text-center border border-zinc-300 rounded-md hover:bg-[#eceef0] transition duration-250 ease-in-out">
+          <Link href="/sobre" className="flex justify-center items-center py-2 px-4 curosr-pointer" onClick={() => setMenuActivo(false)}>
+            Sobre
+          </Link>
+        </li>
+      </ul>
     </nav>
   );
 }
